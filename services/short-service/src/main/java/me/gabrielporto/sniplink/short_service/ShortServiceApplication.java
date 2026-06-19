a5e9d2872973670d7bdcf60df8ecc778e44b098b
Gabriel Porto <gpalmenara@gmail.com>
2026-06-18 22:34:00 -0300

    [Short Service] chore: configura projeto Spring Boot com Maven wrapper

---
+++ services/short-service/src/main/java/me/gabrielporto/sniplink/short_service/ShortServiceApplication.java
@@ -0,0 +1,13 @@
+package me.gabrielporto.sniplink.short_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortServiceApplication.class, args);
+	}
+
+}
