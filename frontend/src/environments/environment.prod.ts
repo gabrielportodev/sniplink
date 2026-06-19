76d1be8d8899b5412b13cd4da275d02f9f42606e
Gabriel Porto <gpalmenara@gmail.com>
2026-06-19 09:27:52 -0300

    [Front End] feat: adiciona configuração de ambientes (dev/prod)

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: '/api',
+  shortBaseUrl: '',
+  analyticsApiUrl: '/analytics-api',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8082/api',
+  shortBaseUrl: 'http://localhost:8082',
+  analyticsApiUrl: 'http://localhost:8083/api',
+};
